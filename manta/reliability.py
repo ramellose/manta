f9f4bf73e377656038deae88c29b679ee4e9e3da
ramellose <lisa.rottjers@kuleuven.be>
2019-03-08 16:26:16 +0100

    bugfix

--- manta/reliability.py
+++ manta/reliability.py
@@ -332,7 +332,7 @@ def robustness(graphclusters, permutations):
             rev_assignment = rev_assignments[i][clusid]
             jaccards.append(jaccard_similarity_score(true_composition, rev_assignment))
         nodejaccards[node] = np.round(norm.interval(0.95, np.mean(jaccards), np.std(jaccards)), 4)
-        CIwidth = nodejaccards[0] - nodejaccards[1]
+        ci_width[node] =  nodejaccards[node][0] - nodejaccards[node][1]
     return clusjaccards, nodejaccards, ci_width
 
 
