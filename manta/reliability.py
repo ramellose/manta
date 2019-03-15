wrong sign

manta/reliability.py
@@ -402,7 +402,7 @@ def robustness(graphclusters, permutations):
             rev_assignment = rev_assignments[i][clusid]
             jaccards.append(jaccard_similarity_score(true_composition, rev_assignment))
         nodejaccards[node] = np.round(norm.interval(0.95, np.mean(jaccards), np.std(jaccards)), 4)
-        ci_width[node] = np.round(nodejaccards[node][0] - nodejaccards[node][1], 4)
+        ci_width[node] = np.round(nodejaccards[node][1] - nodejaccards[node][0], 4)
     return clusjaccards, nodejaccards, ci_width
 
 